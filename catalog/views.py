48b390c6d84b43379454fe591fd4d645b3d1b655
adriano <adriano.qwe32@gmail.com>
2017-07-28 08:43:41 -0300

    pagination

--- catalog/views.py
+++ catalog/views.py
@@ -1,21 +1,33 @@
 # coding=utf-8
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from django.views import generic
 
 
-def product_list(request):
-    context = {'product_list': Product.objects.all()}
-    return render(request, 'catalog/product_list.html', context)
+class ProductListView(generic.ListView):
+    model = Product
+    paginate_by = 3
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'product_list' # optional
+    
+    
+class CategoryListView(generic.ListView):
+#    self.kwargs heve the parans nemmed
+    template_name = 'catalog/category.html'
+    context_object_name = 'product_list' # optional
+    paginate_by = 3
+    
+    def get_catgory(self):
+        return get_object_or_404(Category, slug=self.kwargs['slug'])
+    
+    def get_queryset(self):
+        return Product.objects.filter(category=self.get_catgory())
 
-
-def category(request, slug):
-    category = Category.objects.get(slug=slug)
-    context = {
-            'current_category': category,
-            'product_list': Product.objects.filter(category=category),
-            }
-    return render(request, 'catalog/category.html', context)
-
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['current_category'] = self.get_catgory()
+        return context
+        
 
 def product(request, slug):
     product = Product.objects.get(slug=slug)
